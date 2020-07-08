a2396b43c973c75d483a91cee66e47e247d5604c
Mokarrom Rahman <mrahman0000@gmail.com>
2020-07-07 22:27:04 -0600

    added the border colors for each type and added the pokemon font

--- src/Components/Pokemon.jsx
+++ src/Components/Pokemon.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Carousel, Row, CardColumns, Button, Col } from "react-bootstrap";
-import { Container } from "react-bootstrap";
+import { Container, Row, Image, Button, Col } from "react-bootstrap";
 import { ArrowLeft, ArrowRight } from "react-bootstrap-icons";
+import "bootstrap/dist/css/bootstrap.css";
+import "../styling/style.css";
 //Component which will be used to render a single pokemon
 export default function Pokemon({ number }) {
   const [data, setData] = useState(null);
@@ -16,7 +17,8 @@ export default function Pokemon({ number }) {
   if (data) {
     //find all the sprites and the names of each sprite normalized names
     const spritesArray = getSpriteArray(data.sprites);
-    console.log(spritesArray);
+    //console.log(spritesArray);
+    console.log(data.types);
     return (
       // <div>
       //   <h1>{CapitalizeName(data.name)}</h1>
@@ -26,8 +28,15 @@ export default function Pokemon({ number }) {
       //   ))}
       // </div>
       <>
-        <Container>
-          <h1>{capitalizeName(data.name)}</h1>
+        <Container
+          //https://stackoverflow.com/questions/36209432/reactjs-add-dynamic-class-to-manual-class-names
+          //this can be used to dynamically change the classes
+          className="customBorder customBorder-grass"
+          style={{ width: "15em" }}
+        >
+          <Row className="justify-content-md-center">
+            <label>{capitalizeName(data.name)}</label>
+          </Row>
           <PicSlideShow spritesDescUrlArray={spritesArray} />
 
           {/* {Object.values(data.sprites).map((url, i) => (
@@ -60,39 +69,49 @@ function PicSlideShow({ spritesDescUrlArray }) {
   //   console.log(sprites.length);
   // }, []);
   return (
-    <Container>
-      <Row>
-        <img
+    <>
+      <Row className="justify-content-md-center">
+        <Col>
+          <Button
+            size="sm"
+            onClick={() =>
+              currentSpriteIndex === 0
+                ? setCurrentSpriteIndex(sprites.length - 1)
+                : setCurrentSpriteIndex(currentSpriteIndex - 1)
+            }
+          >
+            <ArrowLeft></ArrowLeft>
+          </Button>
+        </Col>
+
+        <Image
           src={sprites[currentSpriteIndex].url}
           alt={sprites[currentSpriteIndex].description}
-        ></img>
+          fluid
+          thumbnail
+        ></Image>
+        <Col>
+          <Button
+            size="sm"
+            onClick={() =>
+              currentSpriteIndex === sprites.length - 1
+                ? setCurrentSpriteIndex(0)
+                : setCurrentSpriteIndex(currentSpriteIndex + 1)
+            }
+          >
+            <ArrowRight></ArrowRight>
+          </Button>
+        </Col>
+
         {/* {JSON.stringify(currentSprite)} */}
       </Row>
-      <Row>
+      <Row className="justify-content-md-center">
         <label>{sprites[currentSpriteIndex].description}</label>
       </Row>
-      <Row>
-        <Button
-          onClick={() =>
-            currentSpriteIndex === 0
-              ? setCurrentSpriteIndex(sprites.length - 1)
-              : setCurrentSpriteIndex(currentSpriteIndex - 1)
-          }
-        >
-          <ArrowLeft></ArrowLeft>
-        </Button>
-
-        <Button
-          onClick={() =>
-            currentSpriteIndex === sprites.length - 1
-              ? setCurrentSpriteIndex(0)
-              : setCurrentSpriteIndex(currentSpriteIndex + 1)
-          }
-        >
-          <ArrowRight></ArrowRight>
-        </Button>
-      </Row>
-    </Container>
+      {/* <Row className="justify-content-md-center">
+        
+      </Row> */}
+    </>
   );
 }
 // export function PokemonForms(spritesUrlArray) {
@@ -109,9 +128,7 @@ function PicSlideShow({ spritesDescUrlArray }) {
 
 //   return <h1>Hello</h1>;
 // }
-function PokePic({ picURL }) {
-  return <img src={picURL} />;
-}
+
 function capitalizeName(name) {
   return name[0].toUpperCase() + name.slice(1);
 }
@@ -121,24 +138,27 @@ function getSpriteArray(spritesArray) {
   Object.values(spritesArray).map((u, i) =>
     u
       ? retArray.push({
-          description: Object.keys(spritesArray)[i].toString(),
+          description: normalizeDescriptions(
+            Object.keys(spritesArray)[i].toString()
+          ),
           url: u,
         })
       : ""
   );
-
+  Object.values(spritesArray).map((u, i) =>
+    u ? normalizeDescriptions(Object.keys(spritesArray)[i].toString()) : ""
+  );
   return retArray;
 }
 
 //July 7, 2020: there seems to be an issue here so come back to this
-function normalizeDescriptions({ description }) {
-  const desc =
-    capitalizeName(description.slice(0, description.indexof("_"))).toString() +
+function normalizeDescriptions(description) {
+  let desc =
+    capitalizeName(description.substring(0, description.indexOf("_"))) +
     " " +
     capitalizeName(
-      description
-        .slice(description.indexof("_") + 1, description.len)
-        .toString()
+      description.substring(description.indexOf("_") + 1, description.length)
     );
+  //console.log(desc);
   return desc;
 }
