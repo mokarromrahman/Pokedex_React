c624f6eff376907c7d6a35224b99a019f70cd896
Mokarrom Rahman <mrahman0000@gmail.com>
2020-07-07 18:27:53 -0600

    Able to display the images and scroll through them. Next step is formatting the Pokemon component so that it looks nice.

--- src/Components/Pokemon.jsx
+++ src/Components/Pokemon.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Carousel } from "react-bootstrap";
+import { Carousel, Row, CardColumns, Button, Col } from "react-bootstrap";
 import { Container } from "react-bootstrap";
-
+import { ArrowLeft, ArrowRight } from "react-bootstrap-icons";
 //Component which will be used to render a single pokemon
 export default function Pokemon({ number }) {
   const [data, setData] = useState(null);
-  const [sprites, setSprites] = useState([]);
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${number}`)
       .then((res) => res.json())
@@ -26,43 +25,90 @@ export default function Pokemon({ number }) {
       //     <>{url ? <PokePic picURL={url} /> : ""}</> //display non null images
       //   ))}
       // </div>
-      <Container>
-        <h1>{capitalizeName(data.name)}</h1>
+      <>
+        <Container>
+          <h1>{capitalizeName(data.name)}</h1>
+          <PicSlideShow spritesDescUrlArray={spritesArray} />
 
-        {Object.values(data.sprites).map((url, i) => (
-          <>
-            {url ? (
-              <>
-                <PokePic picURL={url} />{" "}
-                <label>{Object.keys(data.sprites)[i]}</label>
-              </>
-            ) : (
-              ""
-            )}
-          </> //display non null images
-        ))}
+          {/* {Object.values(data.sprites).map((url, i) => (
+            <>
+              {url ? (
+                <>
+                  <PokePic picURL={url} />{" "}
+                  <label>{Object.keys(data.sprites)[i]}</label>
+                </>
+              ) : (
+                ""
+              )}
+            </> //display non null images
+          ))} */}
 
-        {/* <PokemonForms spritesUrlArray={data.sprites} /> */}
-      </Container>
+          {/* <PokemonForms spritesUrlArray={data.sprites} /> */}
+        </Container>
+      </>
     );
   }
   return <div>{number} not found.</div>;
 }
 
-export function PokemonForms(spritesUrlArray) {
-  const [urls, setUrls] = useState([]);
-  // useEffect(console.log(Object.values(spritesUrlArray)), []);
-  useEffect(
-    Object.values(spritesUrlArray).map((u) =>
-      Object.values(u) === "null"
-        ? console.log("I was null")
-        : console.log(Object.values(u))
-    ),
-    [spritesUrlArray]
-  );
+function PicSlideShow({ spritesDescUrlArray }) {
+  const [sprites, setSprites] = useState(spritesDescUrlArray);
+  const [currentSpriteIndex, setCurrentSpriteIndex] = useState(0);
+  // useEffect(() => {
+  //   console.log(sprites);
+  //   console.log(currentSpriteIndex);
+  //   console.log(sprites.length);
+  // }, []);
+  return (
+    <Container>
+      <Row>
+        <img
+          src={sprites[currentSpriteIndex].url}
+          alt={sprites[currentSpriteIndex].description}
+        ></img>
+        {/* {JSON.stringify(currentSprite)} */}
+      </Row>
+      <Row>
+        <label>{sprites[currentSpriteIndex].description}</label>
+      </Row>
+      <Row>
+        <Button
+          onClick={() =>
+            currentSpriteIndex === 0
+              ? setCurrentSpriteIndex(sprites.length - 1)
+              : setCurrentSpriteIndex(currentSpriteIndex - 1)
+          }
+        >
+          <ArrowLeft></ArrowLeft>
+        </Button>
 
-  return <h1>Hello</h1>;
+        <Button
+          onClick={() =>
+            currentSpriteIndex === sprites.length - 1
+              ? setCurrentSpriteIndex(0)
+              : setCurrentSpriteIndex(currentSpriteIndex + 1)
+          }
+        >
+          <ArrowRight></ArrowRight>
+        </Button>
+      </Row>
+    </Container>
+  );
 }
+// export function PokemonForms(spritesUrlArray) {
+//   const [urls, setUrls] = useState([]);
+//   // useEffect(console.log(Object.values(spritesUrlArray)), []);
+//   useEffect(
+//     Object.values(spritesUrlArray).map((u) =>
+//       Object.values(u) === "null"
+//         ? console.log("I was null")
+//         : console.log(Object.values(u))
+//     ),
+//     [spritesUrlArray]
+//   );
+
+//   return <h1>Hello</h1>;
+// }
 function PokePic({ picURL }) {
   return <img src={picURL} />;
 }
@@ -75,9 +121,7 @@ function getSpriteArray(spritesArray) {
   Object.values(spritesArray).map((u, i) =>
     u
       ? retArray.push({
-          description: normalizeDescriptions(
-            Object.keys(spritesArray)[i].toString()
-          ),
+          description: Object.keys(spritesArray)[i].toString(),
           url: u,
         })
       : ""
@@ -86,6 +130,7 @@ function getSpriteArray(spritesArray) {
   return retArray;
 }
 
+//July 7, 2020: there seems to be an issue here so come back to this
 function normalizeDescriptions({ description }) {
   const desc =
     capitalizeName(description.slice(0, description.indexof("_"))).toString() +
